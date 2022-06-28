e676a824b26cd488c310e90535d31c6178d87885
Steph <1000p@mail.ru>
2022-06-29 01:35:51 +0700

    add dz

---
+++ Prac3/prac3.js
@@ -0,0 +1,22 @@
+// alert(`Alert ${180 + 2}`)
+// let gg3 = prompt("Как тебя звать?")
+// let gg2 = confirm(`Привет ${gg3}`)
+
+// let gg4 = prompt("Как тебя звать?", "Имя")
+// alert(`Alert ${gg4}`)
+
+// let userName = 'ХаХа'
+// function getname() {
+//     let message = `Привет ${userName}`
+//     alert(message)
+// }
+
+// getname()
+
+let userName = prompt("Как тебя зовут?")
+
+function helloUser(){
+    alert(`Привет ${userName}`)
+}
+
+helloUser()
